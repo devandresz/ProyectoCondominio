73d1613058f8865a44856c3e9acaccdb8316af5f
KevinAr <kevinargueta296@gmail.com>
2026-03-12 12:55:37 -0600

    Reinicializando valores

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,12 +2,11 @@ import 'dotenv/config';
 
 export const {
 	PORT = 1000,
-	DB_USER = 'analisis',
-	DB_PASSWORD = '1234',
+	DB_USER = 'tu_usuario',
+	DB_PASSWORD = 'tu_password',
 	DB_HOST = 'localhost',
-	DB_PORT = 1521,
-	DB_SERVICE = 'orclpdb',
-
+	DB_PORT = 1000,
+	DB_SERVICE = 'tu_servicio',
 	SALT_ROUND = 10,
 	SECRET_JWT_KEY = 'tu_clave_secreta_para_jwt',
 } = process.env;
